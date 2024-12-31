1867a15fd75f20fba8527e47d85f2af609b49979
Onowu Christopher Oyemechi <totiruzi@yahoo.com>
2024-12-31 03:58:54 +0300

    Update solanacrudapp-exports.ts
    Update program Id

--- anchor/src/solanacrudapp-exports.ts
+++ anchor/src/solanacrudapp-exports.ts
@@ -21,7 +21,7 @@ export function getSolanacrudappProgramId(cluster: Cluster) {
     case 'devnet':
     case 'testnet':
       // This is the program ID for the Solanacrudapp program on devnet and testnet.
-      return new PublicKey('coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF')
+      return new PublicKey('8AhaGPHcjPF1RKYqEeHQXRESJoYjuETbfUJ5ErW2CMdN')
     case 'mainnet-beta':
     default:
       return SOLANACRUDAPP_PROGRAM_ID
